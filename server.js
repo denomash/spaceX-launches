e55377edc260da2bf985e4025b306e4b17b04727
Dennis Macharia <41957873+denomash@users.noreply.github.com>
2019-10-11 10:15:22 +0300

    Ft deploy to heroku (#4)
    * Develop (#3)
    
    - add launch items and styling
    
    - add launch component to view each launch (#2)
    
    - add misson key component
    
    - deploy to heroku
    
    - modify package.json
    
    - run build
    
    - add babel to run server
    
    - add babel to dependencies and not devdependencies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import graphqlHTTP from 'express-graphql';
+import path from 'path';
 import schema from './schema';
 
 const app = express();
@@ -18,6 +19,12 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
